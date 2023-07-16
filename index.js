5d0b0772325538df08ab8714a01adad97e3187d3
NatanaelJose <josesaraivamaciel@gmail.com>
2023-07-16 10:57:17 -0300

    WHITELIST

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-const whitelist = ['http://localhost:5000', "https://charming-kashata-ba9538.netlify.app/"];    
+const whitelist = ['http://localhost:5000', "https://spaotify-node.vercel.app/"];    
 const corsOptions = {
     origin: function (origin, callback) {
       if (whitelist.indexOf(origin) !== -1 || !origin) {
