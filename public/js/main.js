a86699d0a2f98df201bcde55d7c6a20b159c6987
NatanaelJose <josesaraivamaciel@gmail.com>
2023-07-17 10:30:47 -0300

    fetch api

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ const totalTime = document.getElementById('total-time');
 const likeButton = document.getElementById('like');
 const usrButton = document.getElementById('user-enter');
 
-const api = fetch('http://localhost:5000/api').then(response => response.json()).then(data => {
+const api = fetch('https://spaotify-node.vercel.app/api').then(response => response.json()).then(data => {
     const dataMusic = data;
 
     // shuffleArray(dataMusic);
