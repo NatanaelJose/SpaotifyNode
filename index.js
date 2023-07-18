7350bb0abccbb412ddc6c50cba83f337faef3f45
NatanaelJose <josesaraivamaciel@gmail.com>
2023-07-17 21:38:27 -0300

    privacy

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
     fontSrc: ["'self'", "https://fonts.gstatic.com"],
-    imgSrc: ["'self'", "https://upload.wikimedia.org", "https://commons.wikimedia.org", "data:"], // Permitir carregar imagens de 'self', 'https://upload.wikimedia.org' e 'data:'
+    imgSrc: ["https://upload.wikimedia.org", "https://commons.wikimedia.org", "data:"], // Permitir carregar imagens de 'self', 'https://upload.wikimedia.org' e 'data:'
     mediaSrc: ["'self'", "https://upload.wikimedia.org", "data:"], // Permitir carregar arquivos de áudio de "https://upload.wikimedia.org"
     scriptSrc: ["'self'", 'sha256-LofIu98g2EQb8PS9m6qIn4S6X4Y1V3lnSeB+FyGqmgU='], // Use o hash SHA-256 do script específico
     styleSrc: ["'self'", "https://fonts.googleapis.com"],
